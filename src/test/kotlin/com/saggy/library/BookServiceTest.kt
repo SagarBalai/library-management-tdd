5c849bf50511fd9fd4d3eed1333e08fb7508c0b6
Sagar Balai <sagra10feb@gmail.com>
2021-05-16 16:20:09 +0200

    BorrowBook in BookService - refactor and cleanup

--- src/test/kotlin/com/saggy/library/BookServiceTest.kt
+++ src/test/kotlin/com/saggy/library/BookServiceTest.kt
@@ -1,15 +1,17 @@
 package com.saggy.library
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 internal class BookServiceTest {
 
+    private val subject = BookService()
+
     @Test
     fun `getBooks should return empty list when no books available`() {
         //given
-        val subject = BookService()
 
         //when
         val result = subject.getBooks()
@@ -21,7 +23,7 @@ internal class BookServiceTest {
     @Test
     fun `getBooks should return list of available books`() {
         //given
-        val subject = BookService()
+        val book = Book("1", "Harry potter", "J L Rowling", 12000.98)
 
         //when
         var result = subject.getBooks()
@@ -30,18 +32,18 @@ internal class BookServiceTest {
         assertTrue(result.isEmpty())
 
         // when
-        subject.addBook(Book("1", "Harry potter", "J L Rowling", 12000.98))
+        subject.addBook(book)
         result = subject.getBooks()
 
         // then
         assertFalse(result.isEmpty())
+        assertEquals(book, result[0])
 
     }
 
     @Test
     fun `borrowBook - should return true and remove book from library for given book is available in library`() {
         // given
-        val subject = BookService()
         val bookId = "book-1"
         subject.addBook(Book(bookId, "Harry potter", "J K Rowling", 12000.98))
         subject.addBook(Book("book-2", "Alone on a Wide, Wide Sea", "Michael Morpurgo", 543.98))
@@ -62,7 +64,6 @@ internal class BookServiceTest {
     @Test
     fun `borrowBook - should return false for given book is available in library`() {
         // given
-        val subject = BookService()
         val bookId = "book-1"
         subject.addBook(Book("book-2", "Alone on a Wide, Wide Sea", "Michael Morpurgo", 543.98))
         subject.addBook(Book("book-3", "O Jerusalem!", "Larry Collins", 200.8))
@@ -72,9 +73,7 @@ internal class BookServiceTest {
 
         // then
         assertFalse(result)
-
-        val availableBooks = subject.getBooks()
-        assertTrue(availableBooks.size == 2)
+        assertTrue(subject.getBooks().size == 2)
     }
 
 
